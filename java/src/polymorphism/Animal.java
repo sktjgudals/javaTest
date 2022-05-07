3079d42d0a67d4af8649312c54bb4fcb5240929f
sktjgudals <81937738+sktjgudals@users.noreply.github.com>
2022-05-07 22:55:43 +0900

    [UPDATE]

--- java/src/polymorphism/Animal.java
+++ java/src/polymorphism/Animal.java
@@ -1,4 +1,5 @@
 package polymorphism;
+import java.util.ArrayList;
 
 class Animal {
 	public void move() {
@@ -10,11 +11,17 @@ class Human extends Animal{
 	public void move() {
 		System.out.println("move the Human");
 	}
+	public void readBook() {
+		System.out.println("human read a book");
+	}
 }
 
 class Tiger extends Animal{
 	public void move() {
 		System.out.println("move the Tiger");
+	}
+	public void hunting() {
+		System.out.println("Tiger hunting dog");
 	}
 }
 
@@ -22,17 +29,56 @@ class Eagle  extends Animal {
 	public void move() {
 		System.out.println("move the Eagle");
 	}
+	
+	public void flying() {
+		System.out.println("Eagle flying the sky");
+	}
 }
 
 class AnimalTest{
-	public static void main(String[] args) {
+	ArrayList<Animal> aniList = new ArrayList<Animal>();
+
+	public static void main(String[] args) {	
 		AnimalTest test= new AnimalTest();
-		test.moveAnimal(new Human());
-		test.moveAnimal(new Tiger());
-		test.moveAnimal(new Eagle());
+		test.addAnimal();
+		System.out.println("down casting");
+		test.testCasting();
+}
+	
+	public void addAnimal() {
+		aniList.add(new Human());
+		aniList.add(new Tiger());
+		aniList.add(new Eagle());
+	
+		for(Animal ani : aniList) {
+		ani.move();
+		}
 	}
+	
+	public void testCasting() {
+		for(int i = 0; i<aniList.size(); i++) {
+			Animal ani = aniList.get(i);
+			if(ani instanceof Human) {
+				Human h = (Human)ani;
+				h.readBook();
+			} 
+			else if (ani instanceof Tiger) {
+				Tiger t = (Tiger) ani;
+				t.hunting();
+			}
+			else if (ani instanceof Eagle) {
+				Eagle e = (Eagle)ani;
+				e.flying();
+			}
+			else {
+				System.out.println("Unsupported type");
+			}
+		}
+	}
+	
 	public void moveAnimal(Animal animal) {
 		animal.move();
 	}
+	
 }
 
